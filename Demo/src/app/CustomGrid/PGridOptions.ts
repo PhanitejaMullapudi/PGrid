727040fd50f6c701c7f9b11019e8be3ff2bf5dbb
PhaniTeja Mullapudi <Phaniteja.M@pbsystems.biz>
2017-02-24 21:58:23 +0530

    Command Buttons added

--- Demo/src/app/CustomGrid/PGridOptions.ts
+++ Demo/src/app/CustomGrid/PGridOptions.ts
@@ -1,9 +1,11 @@
 export class PGridOptions {
+    ShowSelection:boolean;
+    ShowComandButtons:boolean;
     isResizable: boolean;
     GridID: string;
     GridWidth: string;
     ColMinWidth: number;
-    constructor(isResizable: boolean, GridID: string, GridWidth: string, ColMinWidth: number = 30) {
+    constructor(isResizable: boolean, GridID: string, GridWidth: string, ColMinWidth: number = 30,ShowSelection:boolean = false,ShowComandButtons:boolean= false) {
         this.isResizable = isResizable;
         this.GridID = GridID;
         this.GridWidth = GridWidth;
--- Demo/src/app/app.component.ts
+++ Demo/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { PGridOptions, PGridColumn } from './CustomGrid/PGridComponent';
 
 @Component({
@@ -6,8 +6,16 @@ import { PGridOptions, PGridColumn } from './CustomGrid/PGridComponent';
   templateUrl: `./app-component.html`,
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = 'Angular';
+  gridRows: Array<any> = [];
+  ngOnInit()
+  {
+    for(let i=0;i<20;i++)
+    {
+this.gridRows.push({"Header1":"Text 1","Header2":"Text 2","Header3":"Text 3","Header4":"Text 4","Header5":"Text 5"})
+    }
+  }
   gridOptions: PGridOptions = new PGridOptions(true, "testTabe", "100%");
 
   girdColumns: Array<PGridColumn> = new Array<PGridColumn>(
@@ -16,11 +24,5 @@ export class AppComponent {
     { ColumnName: "Header3", HeaderName: "Header 3" },
     { ColumnName: "Header4", HeaderName: "Header 4" },
     { ColumnName: "Header5", HeaderName: "Header 5" });
-
-  gridRows: Array<any> = [{ "Header1": "Text 1", "Header2": "Text 2", "Header3": "Text 3", "Header4": "Text 4", "Header5": "Text 5" },
-  { "Header1": "Text 1", "Header2": "Text 2", "Header3": "Text 3", "Header4": "Text 4", "Header5": "Text 5" },
-  { "Header1": "Text 1", "Header2": "Text 2", "Header3": "Text 3", "Header4": "Text 4", "Header5": "Text 5" },
-  { "Header1": "Text 1", "Header2": "Text 2", "Header3": "Text 3", "Header4": "Text 4", "Header5": "Text 5" }];
-
 
 }
